b3ce995fe1ad59e832b9ed0e84df2fe974e2bce5
Arun <aruntnp@gmail.com>
2018-09-26 18:35:14 +0530

    Office 6:30 - 26-06-18: Tuesday
    Done until sign up

--- FIRST_PROJECT/FIRST_PROJECT/settings.py
+++ FIRST_PROJECT/FIRST_PROJECT/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     #MY_APPS
     'boards',
+    'accounts'
 ]
 
 MIDDLEWARE = [
--- FIRST_PROJECT/FIRST_PROJECT/urls.py
+++ FIRST_PROJECT/FIRST_PROJECT/urls.py
@@ -17,10 +17,13 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from boards import views
+from accounts import views as accounts_views
 
 urlpatterns = [
 	url(r'^$', views.home, name='home'),
     url(r'^admin/', admin.site.urls),
+    url(R'^signup/$', accounts_views.signup, name='signup'),
+
 
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name = 'board_topics'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topics, name='new_topics'),
