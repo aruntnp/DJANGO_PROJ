b4cf96cfe6bc698b2a13a8f1eadc2d4dc46bbbfb
Arun <aruntnp@gmail.com>
2018-10-01 18:29:24 +0530

    From Office_01-10-18_eve
    Fixed the page bugs but yet to develop.

--- FIRST_PROJECT/FIRST_PROJECT/urls.py
+++ FIRST_PROJECT/FIRST_PROJECT/urls.py
@@ -37,20 +37,13 @@ urlpatterns = [
         name='password_reset'
         ),
     url(r'^reset/done/$',
-        auth_views.PasswordResetDoneView.as_view(
-            template_name='pass_reset/password_reset_done.html'
-        ),
-        name='password_reset_done'
-        ),
+        auth_views.PasswordResetDoneView.as_view(template_name='pass_reset/password_reset_done.html'),
+        name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        auth_views.PasswordResetConfirmView.as_view(
-            template_name='pass_reset/password_reset_confirm.html'
-        ),
+        auth_views.PasswordResetConfirmView.as_view(template_name='pass_reset/password_reset_confirm.html'),
         name='password_reset_confirm'),
     url(r'^reset/complete/$',
-        auth_views.PasswordResetCompleteView.as_view(
-            template_name='pass_reset/password_reset_complete.html'
-        ),
+        auth_views.PasswordResetCompleteView.as_view(template_name='pass_reset/password_reset_complete.html'),
         name='password_reset_complete'),
 
     # RESET PASSWORD in SETTING PAGE by USER
